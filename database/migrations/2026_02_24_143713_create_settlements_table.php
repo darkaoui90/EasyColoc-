ed2e26bc0e6c28f9ab3f288180832465bc28dee2
Mohammed Darkaoui <darkaouimeed12@gmail.com>
2026-02-26 11:31:47 +0000

    fix: remove duplicate timestamps in settlements migration

--- database/migrations/2026_02_24_143713_create_settlements_table.php
+++ database/migrations/2026_02_24_143713_create_settlements_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('settlements', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
             $table->foreignId('colocation_id')->constrained('colocations')->cascadeOnDelete();
 
             $table->foreignId('from_user_id')->constrained('users')->cascadeOnDelete();
